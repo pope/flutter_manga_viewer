fbdeec374e487e4f0f83a9ec4e80c063a122bec0
K. Adam Christensen <pope@shifteleven.com>
2023-07-30 23:30:39 -0700

    Remove unused import

--- lib/providers.dart
+++ lib/providers.dart
@@ -5,7 +5,6 @@ import 'dart:typed_data';
 import 'package:archive/archive_io.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_manga_viewer/models/book.dart';
 import 'package:flutter_manga_viewer/models/library.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
